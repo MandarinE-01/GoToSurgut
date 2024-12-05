3b9809284f117cfc1889015560edc0dbc498a412
MandarinE-01 <152597524+MandarinE-01@users.noreply.github.com>
2024-12-05 22:54:16 +0500

    Добавлена Стамина
    Добавлена трата стамины при беге, прыжке. Добавлено восстановление стамины

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,31 +1,43 @@
 using Fusion;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMove : NetworkBehaviour
 {
     public Camera CameraPlayer;
 
+
     [SerializeField] private float _gravity = 9.81f;
     [SerializeField] private float _playerSpeed = 6f;
-    [SerializeField] private float _junpForce = 5f;
+    [SerializeField] private float _jumpForce = 5f;
+    [SerializeField] private float _runSpeed = 1.5f;
+    [SerializeField] private GameObject _head;
 
+    [Header("Stamina")]
+    [SerializeField] private float _stamin = 10.0f;
+    [SerializeField] private float _delayToRecovery = 1.5f;
+    [SerializeField] private float _spendStaminToRun = 1.2f;
+    [SerializeField] private float _spendStaminToJump = 2.3f;
+    bool canSpend = true;
 
     private CharacterController _cController;
     private bool _isJumping = false;
     private Vector3 _velocity;
     private bool _canMove;
+    private float _staminMax;
 
     private void Awake()
     {
         _cController = GetComponent<CharacterController>();
+        _staminMax = _stamin;
     }
     public override void Spawned()
     {
         if (HasStateAuthority)
         {
             CameraPlayer = Camera.main;
-            CameraPlayer.GetComponent<MoveCamera>().PlayerTarget = transform;
+            CameraPlayer.GetComponent<MoveCamera>().PlayerTarget = _head.transform;
             StartCoroutine(WaitForStart());
         }
     }
@@ -46,13 +58,19 @@ public class PlayerMove : NetworkBehaviour
         }
         Quaternion cameraRotationY = Quaternion.Euler(0, CameraPlayer.transform.rotation.eulerAngles.y, 0);
         Vector3 move = cameraRotationY * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Runner.DeltaTime * _playerSpeed;
-
-        _velocity.y += -_gravity * Runner.DeltaTime;
-        if (_isJumping && _cController.isGrounded)
+        if (Input.GetKey(KeyCode.LeftShift) && canSpend)
         {
-            _velocity.y += _junpForce;
+            move *= _runSpeed;
+        }
+        transform.rotation = cameraRotationY;
+        _velocity.y += -_gravity * Runner.DeltaTime;
+        if (_isJumping && _cController.isGrounded && _stamin >= _spendStaminToJump)
+        {
+            _velocity.y += _jumpForce;
+            SpendStamin(true);
             _isJumping = false;
         }
+        SpendStamin(false);
         _cController.Move(move + _velocity * Runner.DeltaTime);
     }
 
@@ -61,4 +79,46 @@ public class PlayerMove : NetworkBehaviour
         yield return new WaitForFixedUpdate();
         _canMove = true;
     }
+    IEnumerator BlockStamin()
+    {
+        yield return new WaitForSeconds(_delayToRecovery);
+        while (_stamin >= _staminMax / 4)
+        {
+            _stamin += Runner.DeltaTime;
+        }
+        yield return null;
+    }
+    private bool SpendStamin(bool isJumping)
+    {
+        if (_stamin <= 0)
+        {
+            canSpend = false;
+            _stamin = 0.001f;
+            StartCoroutine(BlockStamin());
+        }
+        if (canSpend)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                _stamin -= _spendStaminToRun * Runner.DeltaTime;
+                return true;
+            }
+            else if (isJumping)
+            {
+                _stamin -= _spendStaminToJump;
+                return true;
+            }
+            else
+            {
+                _stamin += Runner.DeltaTime;
+                if (_stamin >= _staminMax)
+                {
+                    _stamin = _staminMax;
+                }
+            }
+        }
+        return false;
+    }
 }
+
+
